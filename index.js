271136021fed05c1e26f305eb8e39c251e8248df
Anano Topuria <anano.topuriaa@gmail.com>
2023-12-19 17:15:48 +0400

    feat: add basic page layout and implement modal window

---
+++ index.js
@@ -0,0 +1,7 @@
+import { openModalWindow } from '/js/modal.js';
+import { closeModalWindow } from '/js/modal.js';
+import { overlayModal } from '/js/modal.js';
+
+openModalWindow();
+closeModalWindow();
+overlayModal();
